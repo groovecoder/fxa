cdeb7d962d265aae5ad318071c2ebe5f0e8657ed
Vijay Budhram <vbudhram@gmail.com>
2023-01-24 13:49:02 -0500

    feat(react): Add security events to settings page

---
+++ packages/functional-tests/tests/react-conversion/cannotCreateAccount.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '../../lib/fixtures/standard';
+import { BaseTarget } from '../../lib/targets/base';
+
+function getReactFeatureFlagUrl(target: BaseTarget, path: string) {
+  return `${target.contentServerUrl}${path}?showReactApp=true`;
+}
+
+test.describe('react-conversion', () => {
+  test(' Cannot create account', async ({ page, target }) => {
+    await page.goto(getReactFeatureFlagUrl(target, '/cannot_create_account'));
+    expect(page.isVisible('root')).toBeTruthy();
+  });
+});
